0d36609848822f9267e81a1b339018d1c7cf0037
Daniil Istomin <istomin.05.3@gmail.com>
2026-04-28 12:38:51 +0700

    Initial commit

---
+++ src/main/java/com/codbid/telemetry/aggregator/TelemetryAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.codbid.telemetry.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemetryAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codbid/telemetry/aggregator/TelemetryAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codbid.telemetry.aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
